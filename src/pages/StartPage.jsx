7931365aa9f2751c438adeeb34bc3d57c253b61e
tony24123 <tjdduswns01@naver.com>
2025-02-13 17:16:45 +0900

    시작버튼 클릭시 이동동

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import '../App.css'
+import { useNavigate } from "react-router-dom";
 
 
 const StartPage = () => {
 
+  //버튼 클릭시 이동동
+  const navigate = useNavigate();
+
   //버튼 클릭 이벤트
   const handleStartClick = () => {
     console.log("해몽을 시작합니다!");
+    navigate('/character')
   };
 
   return (
