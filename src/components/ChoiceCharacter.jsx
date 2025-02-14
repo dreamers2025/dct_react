2f9bccc7b72b41ef02c9162cc5288e6b3da5924c
tony24123 <tjdduswns01@naver.com>
2025-02-14 14:42:03 +0900

    img update

--- src/components/ChoiceCharacter.jsx
+++ src/components/ChoiceCharacter.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './ChoiceCharacter.css'
+import ChristianImg from '../image/freepik__a-photorealistic-portrait-of-a-middle-eastern-man-__83040-removebg-preview.png'
+import MonkImg from '../image/freepik__the-style-is-modern-and-it-is-a-detailed-illustrat__83038-removebg-preview.png'
 
 //ai 캐릭터 선택 컴포넌트
 const ChoiceCharacter = () => {
   return (
-    <h1>캐릭터 설정 페이지입니다.</h1>
+    <div className='box'>
+
+      <div className='imgcard'>
+        <img src={ChristianImg} alt="" />
+      </div>
+
+      <div className='imgcard'>
+        <img src={MonkImg} alt="" />
+      </div>      
+
+    </div>
   )
 }
 
