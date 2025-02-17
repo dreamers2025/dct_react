c46190a52669610e478e97144c7cb5f4a97e8a19
tony24123 <tjdduswns01@naver.com>
2025-02-17 17:06:15 +0900

    router 필요없는 부분 삭제

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -1,12 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import ChoiceCharacterPage from "../components/ChoiceCharacter";
-import UserExperiencePage from "../components/UserExperience";
-import DreamContentsPage from "../components/DreamContents";
 import StartPage from "../pages/StartPage";
 import RootLayout from "../layouts/RootLayout";
-import ChoiceCharacter from "../components/ChoiceCharacter";
-import UserExperience from "../components/UserExperience";
-import DreamContents from "../components/DreamContents";
+
 
 //라우터 설정
 export const router = createBrowserRouter([
@@ -21,18 +16,6 @@ export const router = createBrowserRouter([
         path: '/',
         element: <StartPage />,
       },  
-      {
-        path: '/character',
-        element: <ChoiceCharacter />,
-      },
-      {
-        path: '/experience',
-        element: <UserExperience/>,
-      },
-      {
-        path: '/dreamContents',
-        element: <DreamContents />,
-      },
     ]
   },  
 ]);
