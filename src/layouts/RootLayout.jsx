7046043e5f67f6080161308b7eb7ef623af372a7
inhyeokoh <oredtiger@naver.com>
2025-02-18 16:32:38 +0900

    add main nav

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import './RootLayout.css';
+import MainNavigation from "./MainNavigation.jsx";
 
 const RootLayout = () => {
   return (
     <div className="main-page">
-      
+        <MainNavigation />
         {/* 바뀌는부분(동적 컴포넌트)이 들어갈 자리 */}
-        <Outlet />      
+        <Outlet />
       
-      <div className="stars"></div>
-      <button className='login-btn'>로그인</button>
+        <div className="stars"></div>
     </div>
   )
 }
