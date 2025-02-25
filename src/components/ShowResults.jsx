d449f32a96a7e97fc1edefbf372938981a937b57
tony24123 <tjdduswns01@naver.com>
2025-02-25 23:34:41 +0900

    User등급에 따른 블러처리리

--- src/components/ShowResults.jsx
+++ src/components/ShowResults.jsx
@@ -5,10 +5,18 @@ import SelectedCard from './SelectedCard'
 //폰트어썸 (자물쇠 아이콘 사용)
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLock } from '@fortawesome/free-solid-svg-icons'; // 자물쇠 아이콘
+import { useAuth } from "./auth/AuthProvider";
 
 const ShowResults = ({ filteredCard, responseResults, stepToHome }) => {
   console.log(responseResults);
 
+  const {user}=useAuth(); // 로그인 유저 정보
+  console.log(user);
+
+  const accessToken = localStorage.getItem("accessToken");
+  console.log(accessToken);
+  
+  
   // const[userGrade,setUserGrade] = useState('userGrade');  //유료 무료 구분에 따라 변경
 
   return (
@@ -19,12 +27,20 @@ const ShowResults = ({ filteredCard, responseResults, stepToHome }) => {
         </div>
         <div className="result-content">
           <p className="content-title">해몽</p>
-          <div className="blur-wrapper">  {/* 무료회원 블러 처리 */}
-            <p className="interpretation-content">{responseResults.gemini.content}</p> {/* 해몽 */}
-            <div className="lock-icon"> {/* 자물쇠 */}
-              <FontAwesomeIcon icon={faLock} size="3x" />  
-            </div>
-          </div>          
+
+          <div className="blur-wrapper">  {/* user의 등급이 null이라면 자물쇠로 해몽 내용 블러 처리 */}
+            {user === null ? (
+              <>
+                <p className="interpretation-content">{responseResults.gemini.content}</p>
+                <div className="lock-icon">
+                  <FontAwesomeIcon icon={faLock} size="3x" />
+                </div>
+              </>
+            ) : (
+              <p className="interpretation-content">{responseResults.gemini.content}</p>
+            )}
+          </div>
+            
           <p className="content-title">요약</p>
           <p>{responseResults.gemini.summary}</p> {/* 요약 */}
         </div>
